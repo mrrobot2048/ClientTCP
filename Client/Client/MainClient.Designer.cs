0e52d0457ce9663ff56030031be14e70b52f92a9
mrrobot2048 <53491666+mrrobot2048@users.noreply.github.com>
2019-09-27 18:10:03 -0300

    Socket simple de tipo Cliente, de conexión inversa de tipo sincrónico...

---
+++ Client/Client/MainClient.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Client
+{
+    partial class MainClient
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtServerResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtServerResult
+            // 
+            this.txtServerResult.Location = new System.Drawing.Point(12, 12);
+            this.txtServerResult.Name = "txtServerResult";
+            this.txtServerResult.Size = new System.Drawing.Size(198, 20);
+            this.txtServerResult.TabIndex = 0;
+            // 
+            // MainClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 301);
+            this.Controls.Add(this.txtServerResult);
+            this.Name = "MainClient";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtServerResult;
+    }
+}
+
---
+++ Client/Client/MainClient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace Client
+{
+    public partial class MainClient : Form
+    {
+        public static int port = 8888;
+        public static TcpListener TcpListener;
+        public static Thread conn = new Thread(waitConnection);
+
+        public static void waitConnection()
+        {
+            try
+            {
+                TcpListener = new TcpListener(IPAddress.Any, port);
+                String localHostName = Dns.GetHostName();
+
+
+                TcpListener.Start();
+                TcpListener.AcceptSocket();
+                MessageBox.Show("Connection Successfull!! " + localHostName);
+            }
+            catch (Exception err) { return; }
+
+        }
+        public MainClient()
+        {
+            InitializeComponent();
+            txtServerResult.Text = "My Ipuerto >> " + port;
+        }
+
+        private void MainClient_Load(object sender, EventArgs e)
+        {
+            conn.Start();
+        }
+    }
+}
