ad9b0332183403b9a4738894fea95b7538345bcf
ESTRELLA DEL SUR <ESTRELLA DEL SUR@DESKTOP-3NKCNJ7>
2019-10-09 15:45:12 -0300

    Se agrega a la lista nombre del usario...

--- Client/Client/MainClient.Designer.cs
+++ Client/Client/MainClient.Designer.cs
@@ -30,18 +30,22 @@
         {
             this.txtServerResult = new System.Windows.Forms.TextBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // txtServerResult
             // 
             this.txtServerResult.BackColor = System.Drawing.Color.Black;
             this.txtServerResult.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtServerResult.Font = new System.Drawing.Font("ArcadeClassic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtServerResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtServerResult.ForeColor = System.Drawing.Color.Black;
             this.txtServerResult.Location = new System.Drawing.Point(620, 273);
             this.txtServerResult.Name = "txtServerResult";
             this.txtServerResult.ReadOnly = true;
-            this.txtServerResult.Size = new System.Drawing.Size(24, 16);
+            this.txtServerResult.Size = new System.Drawing.Size(24, 17);
             this.txtServerResult.TabIndex = 0;
             this.txtServerResult.Visible = false;
             this.txtServerResult.TextChanged += new System.EventHandler(this.txtServerResult_TextChanged);
@@ -57,16 +61,45 @@
             this.listBox1.Size = new System.Drawing.Size(282, 91);
             this.listBox1.TabIndex = 1;
             // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 142);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(413, 119);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name PC";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "IP/PC";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Port";
+            // 
             // MainClient
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(656, 301);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.txtServerResult);
             this.Name = "MainClient";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainClient_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -76,6 +109,10 @@
 
         private System.Windows.Forms.TextBox txtServerResult;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
     }
 }
 
--- Client/Client/MainClient.cs
+++ Client/Client/MainClient.cs
@@ -5,14 +5,18 @@ using System.IO;
 using System.Net;
 using System.Threading;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
 
 namespace Client
 {
     public partial class MainClient : Form
     {
 
-        Socket server = null;
+        public Socket server;
         public static int numlines = 0;
+        public byte[] bytes = new byte[1024];
+      
 
         public MainClient()
         {
@@ -21,16 +25,57 @@ namespace Client
             Socket client = null;
             try
             {
-
+                
+                
 
                 server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                server.Bind(new IPEndPoint(IPAddress.Any, 8888));
+                server.Bind(new IPEndPoint(IPAddress.Parse("192.168.2.31"), 8888));
+                string hostName = Dns.GetHostName();
                 server.Listen(100);
                 txtServerResult.Text = "Esperando conexión del servidor... ";
                 client = server.Accept();
+
+                bytes = new byte[1024];
+                int bytesRec = client.Receive(bytes);
+                
+
+
                 txtServerResult.Text = "Nueva conexión de servidor aceptada ... ";
-                txtServerResult.Text = "Datos Server........................." + client.LocalEndPoint;
+                
                 txtServerResult.Text = "Datos Cliente........................" + client.RemoteEndPoint;
+                txtServerResult.Text = "Datos Cliente........................" + Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                listView1.View = View.Details;
+                listView1.GridLines = true;
+                listView1.FullRowSelect = true;
+
+                //Add column header
+                listView1.Columns.Add("Name PC");
+                listView1.Columns.Add("IP/PC");
+                listView1.Columns.Add("Port");
+
+                //Add items in the listview
+                string[] arr = new string[4];
+                ListViewItem itm;
+
+                //Add first item
+                arr[0] = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                arr[1] = "hola";
+                arr[2] = "esto";
+                itm = new ListViewItem(arr);
+                listView1.Items.Add(itm);
+
+                //Add second item
+                arr[0] = "ella";
+                arr[1] = "perro";
+                arr[2] = "ppp";
+                itm = new ListViewItem(arr);
+                listView1.Items.Add(itm);
+
+
+
+
+
             }
 
 
@@ -49,6 +94,9 @@ namespace Client
             }
             Console.Read();
         }
+
+
+
         private void txtServerResult_TextChanged(object sender, EventArgs e)
         {
             numlines = txtServerResult.Lines.Count();
@@ -58,6 +106,17 @@ namespace Client
                 listBox1.Items.Add(txtServerResult.Lines[lines - 1]);
                 lines++;
             }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void MainClient_Load(object sender, EventArgs e)
+        {
+            
         }
     }
 }
